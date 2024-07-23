0e05836d880b6f497031abd1e58e509aa3c9b6df
Matthieu Casanova <public@kpouer.com>
2024-07-23 21:08:57 +0200

    simplification and unit test

--- src/ceos/command/direct/zoom.rs
+++ src/ceos/command/direct/zoom.rs
@@ -1,5 +1,6 @@
 use eframe::epaint::FontId;
 
+#[derive(Debug, PartialEq)]
 pub(crate) struct Zoom {
     size: f32,
 }
@@ -10,18 +11,45 @@ impl TryFrom<&str> for Zoom {
     fn try_from(command: &str) -> Result<Self, Self::Error> {
         if let Some(stripped) = command.strip_prefix("zoom ") {
             if let Ok(size) = stripped.parse::<f32>() {
-                Ok(Zoom { size })
-            } else {
-                Err(())
+                return Ok(Zoom { size });
             }
-        } else {
-            Err(())
         }
+        return Err(());
     }
 }
 
 impl Zoom {
     pub(crate) fn get_font_id(&self) -> FontId {
         FontId::new(self.size, egui::FontFamily::Monospace)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_try_from() -> anyhow::Result<(), ()> {
+        let result = Zoom::try_from("zoom 20")?;
+        assert_eq!(Zoom { size: 20.0 }, result);
+        Ok(())
+    }
+
+    #[test]
+    fn test_try_from_invalid() -> anyhow::Result<(), ()> {
+        assert!(Zoom::try_from("zoo m 20").is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_try_from_invalid2() -> anyhow::Result<(), ()> {
+        assert!(Zoom::try_from("zoom").is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn test_try_from_invalid3() -> anyhow::Result<(), ()> {
+        assert!(Zoom::try_from("zoom a").is_err());
+        Ok(())
     }
 }
