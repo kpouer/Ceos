ea3033f85e2bb3b723b57020243d06d57706a66a
Matthieu Casanova <public@kpouer.com>
2024-07-23 21:20:38 +0200

    add reset zoom

--- src/ceos/command/direct/zoom.rs
+++ src/ceos/command/direct/zoom.rs
@@ -1,3 +1,4 @@
+use crate::ceos::textarea::textareaproperties::DEFAULT_LINE_HEIGHT;
 use eframe::epaint::FontId;
 
 #[derive(Debug, PartialEq)]
@@ -5,10 +6,21 @@ pub(crate) struct Zoom {
     size: f32,
 }
 
+impl Default for Zoom {
+    fn default() -> Self {
+        Self {
+            size: DEFAULT_LINE_HEIGHT,
+        }
+    }
+}
+
 impl TryFrom<&str> for Zoom {
     type Error = ();
 
     fn try_from(command: &str) -> Result<Self, Self::Error> {
+        if command.trim() == "zoom reset" {
+            return Ok(Default::default());
+        }
         if let Some(stripped) = command.strip_prefix("zoom ") {
             if let Ok(size) = stripped.parse::<f32>() {
                 return Ok(Zoom { size });
@@ -32,6 +44,18 @@ mod tests {
     fn test_try_from() -> anyhow::Result<(), ()> {
         let result = Zoom::try_from("zoom 20")?;
         assert_eq!(Zoom { size: 20.0 }, result);
+        Ok(())
+    }
+
+    #[test]
+    fn test_try_from_reset() -> anyhow::Result<(), ()> {
+        let result = Zoom::try_from("zoom reset")?;
+        assert_eq!(
+            Zoom {
+                size: DEFAULT_LINE_HEIGHT
+            },
+            result
+        );
         Ok(())
     }
 
