ada2ff77bb328d0a7c6f6d57ed40146bdd24d900
Matthieu Casanova <public@kpouer.com>
2024-10-01 22:21:06 +0200

    move all Ceos impl to the same file

--- src/ceos/command/mod.rs
+++ src/ceos/command/mod.rs
@@ -1,57 +1,11 @@
 use std::fmt::Display;
 
-use log::{debug, info};
-
-use filter::columnfilter::ColumnFilter;
-use filter::linefilter::LineFilter;
-
 use crate::ceos::buffer::Buffer;
-use crate::ceos::command::filter::linedrop::LineDrop;
 use crate::ceos::gui::textpane::renderer::Renderer;
-use crate::ceos::Ceos;
-use crate::event::Event;
-use search::Search;
 
 pub(crate) mod direct;
-mod filter;
+pub(crate) mod filter;
 pub(crate) mod search;
-
-impl Ceos {
-    pub(crate) fn try_search(&mut self) -> bool {
-        if let Ok(mut search) = Search::try_from(self.command_buffer.as_str()) {
-            search.init(&self.textarea_properties.buffer);
-            self.search_panel.search = search;
-            return true;
-        }
-        false
-    }
-
-    pub(crate) fn try_filter_command(&mut self) {
-        let command_str = self.command_buffer.as_str();
-        if let Ok(command) = LineFilter::try_from(command_str) {
-            self.current_command = Some(Box::new(command));
-        } else if let Ok(command) = ColumnFilter::try_from(command_str) {
-            self.current_command = Some(Box::new(command));
-        } else if let Ok(command) = LineDrop::try_from(command_str) {
-            self.current_command = Some(Box::new(command));
-        } else {
-            self.current_command = None;
-        }
-
-        if let Some(command) = &self.current_command {
-            debug!("Found command {}", command);
-        }
-    }
-
-    pub(crate) fn execute_command(&mut self) {
-        if let Some(command) = self.current_command.take() {
-            info!("Execute command {}", command);
-            command.execute(&mut self.textarea_properties.buffer);
-        } else if let Ok(command) = Event::try_from(self.command_buffer.as_str()) {
-            self.sender.send(command).unwrap();
-        }
-    }
-}
 
 pub(crate) trait Command: Renderer + Display {
     fn execute(&self, buffer: &mut Buffer);
