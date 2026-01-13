cac2ef64b66443f3021c03e1e68a505bd9797e24
Matthieu Casanova <chocolat.mou@gmail.com>
2026-01-13 22:36:09 +0100

    implement PartialEq and Eq and PartialOrd

--- src/ceos/gui/textpane/position.rs
+++ src/ceos/gui/textpane/position.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq, Eq)]
 pub(crate) struct Position {
     pub(crate) line: usize,
     pub(crate) column: usize,
@@ -9,5 +9,17 @@ pub(crate) struct Position {
 impl Display for Position {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", format!("line: {}, column: {}", self.line, self.column))
+    }
+}
+
+impl PartialOrd for Position {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if self.line < other.line {
+            Some(std::cmp::Ordering::Less)
+        } else if self.line == other.line {
+            Some(self.column.cmp(&other.column))
+        } else {
+            Some(std::cmp::Ordering::Greater)
+        }
     }
 }
