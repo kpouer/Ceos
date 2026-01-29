46f09ce65713a53eb4f98d57f93490a1091311e5
Matthieu Casanova <chocolat.mou@gmail.com>
2026-01-29 23:06:04 +0100

    refactor

--- src/ceos/tools/misc_tool.rs
+++ src/ceos/tools/misc_tool.rs
@@ -3,14 +3,19 @@ use std::io::{Read, Seek, SeekFrom};
 
 const GZIP_MAGIC: [u8; 2] = [0x1F, 0x8B];
 
-pub(crate) fn is_gzip(buffer: &mut (impl Read + Seek)) -> bool {
-    let mut buf = [0; 2];
-    let result = match buffer.read_exact(&mut buf) {
-        Ok(_) => buf == GZIP_MAGIC,
-        Err(_) => false,
+pub(crate) fn is_gzip<R>(reader: &mut R) -> bool
+where
+    R: Read + Seek,
+{
+    let mut magic = [0; GZIP_MAGIC.len()];
+    let start_pos = reader.stream_position().ok().unwrap_or(0);
+    let is_gzip = if reader.read_exact(&mut magic).is_ok() {
+        magic == GZIP_MAGIC
+    } else {
+        false
     };
-    let _ = buffer.seek(SeekFrom::Start(0));
-    result
+    let _ = reader.seek(SeekFrom::Start(start_pos));
+    is_gzip
 }
 
 pub(crate) fn gzip_uncompressed_size_fast(path: &std::path::Path) -> std::io::Result<u32> {
