c7d83d003ab0b4f5f84fe738c4d0facf7f991604
Matthieu Casanova <chocolat.mou@gmail.com>
2026-03-01 10:50:14 +0100

    do not clone the query

--- src/ceos/search/simple_search_matcher.rs
+++ src/ceos/search/simple_search_matcher.rs
@@ -1,18 +1,19 @@
 use crate::ceos::search::SearchMatcher;
+use std::borrow::Cow;
 
 #[derive(Debug)]
-pub(crate) struct SimpleSearchMatcher {
-    query: String,
+pub(crate) struct SimpleSearchMatcher<'a> {
+    query: Cow<'a, str>,
     case_insensitive: bool,
     whole_words: bool,
 }
 
-impl SimpleSearchMatcher {
-    pub(crate) fn new(query: &str, case_insensitive: bool, whole_words: bool) -> Self {
+impl<'a> SimpleSearchMatcher<'a> {
+    pub(crate) fn new(query: &'a str, case_insensitive: bool, whole_words: bool) -> Self {
         let query_text = if case_insensitive {
-            query.to_lowercase()
+            Cow::Owned(query.to_lowercase())
         } else {
-            query.to_string()
+            Cow::Borrowed(query)
         };
         Self {
             query: query_text,
@@ -22,7 +23,7 @@ impl SimpleSearchMatcher {
     }
 }
 
-impl SearchMatcher for SimpleSearchMatcher {
+impl SearchMatcher for SimpleSearchMatcher<'_> {
     fn search(&self, line_text: &str, from_col: usize) -> Option<(usize, usize)> {
         let search_text = if self.case_insensitive {
             line_text.to_lowercase()
@@ -31,23 +32,25 @@ impl SearchMatcher for SimpleSearchMatcher {
         };
 
         let search_slice = &search_text[from_col..];
-        search_slice.find(&self.query).and_then(|found_idx| {
-            let actual_idx = from_col + found_idx;
-            let match_len = self.query.len();
-            if self.whole_words {
-                let before = if actual_idx == 0 {
-                    ' '
-                } else {
-                    line_text.chars().nth(actual_idx - 1).unwrap_or(' ')
-                };
-                let after = line_text.chars().nth(actual_idx + match_len).unwrap_or(' ');
-                if (before.is_alphanumeric() || before == '_')
-                    || (after.is_alphanumeric() || after == '_')
-                {
-                    return None;
+        search_slice
+            .find(self.query.as_ref())
+            .and_then(|found_idx| {
+                let actual_idx = from_col + found_idx;
+                let match_len = self.query.len();
+                if self.whole_words {
+                    let before = if actual_idx == 0 {
+                        ' '
+                    } else {
+                        line_text.chars().nth(actual_idx - 1).unwrap_or(' ')
+                    };
+                    let after = line_text.chars().nth(actual_idx + match_len).unwrap_or(' ');
+                    if (before.is_alphanumeric() || before == '_')
+                        || (after.is_alphanumeric() || after == '_')
+                    {
+                        return None;
+                    }
                 }
-            }
-            Some((actual_idx, actual_idx + match_len))
-        })
+                Some((actual_idx, actual_idx + match_len))
+            })
     }
 }
