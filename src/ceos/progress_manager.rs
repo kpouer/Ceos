01a9e6f93776eb30c8a1af7cff353faed6695a84
Matthieu Casanova <chocolat.mou@gmail.com>
2026-04-19 10:31:27 +0200

    fix: update bounds check in `percent` calculation to include equality

--- src/ceos/progress_manager.rs
+++ src/ceos/progress_manager.rs
@@ -55,7 +55,7 @@ pub(crate) struct Progress {
 
 impl Progress {
     pub(crate) const fn percent(&self) -> f32 {
-        if self.current > self.max {
+        if self.current >= self.max {
             1.0
         } else {
             self.current as f32 / self.max as f32
